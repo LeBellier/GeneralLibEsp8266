f8d6b3dc805f8c31b365382d92b576ea116d7ee5
LeBellier <Brusuf@orange.fr>
2017-01-21 00:11:11 +0100

    Ajout du point d'acces

--- src/WifiManager.h
+++ src/WifiManager.h
@@ -16,37 +16,53 @@ void initWIFI() {
 
 	boolean wifiFounded = false;
 	// Set WiFi to station mode and disconnect from an AP if it was previously connected
-	WiFi.mode(WIFI_STA);
+	WiFi.enableSTA(true);
 	WiFi.disconnect();
 	delay(100);
 	// WiFi.scanNetworks will return the number of networks found
 	int n = WiFi.scanNetworks();
 	if (n != 0) {
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < nbSSID; ++j) {
+		int i = 0, j = 0;
+		while (i < n && !wifiFounded) {
+			while (j < nbSSID && !wifiFounded) {
 				if (((ESP8266WiFiScanClass) WiFi).SSID(i) == SSIDs[j]) {
 					ssid = SSIDs[j];
 					password = passewords[j];
 					wifiFounded = true;
-					break;
 				}
+				j++;
 			}
-			if (wifiFounded) {
-				WiFi.begin(ssid, password);
-				DEBUG_INIT_PRINTLN("");
-				// Wait for connection
-				while (WiFi.status() != WL_CONNECTED) {
-					delay(500);
-					DEBUG_INIT_PRINT(".");
-				}
-				String sIP = WiFi.localIP().toString();
-				DEBUG_INIT_PRINTLN("");
-				DEBUG_INIT_PRINT("Connected to ");
-				DEBUG_INIT_PRINTLN(ssid);
-				DEBUG_INIT_PRINT("IP address: ");
-				DEBUG_INIT_PRINTLN(sIP);
-				break;
+			i++;
+		}
+
+		if (wifiFounded) {
+			WiFi.begin(ssid, password);
+			DEBUG_INIT_PRINTLN("");
+			// Wait for connection
+			while (WiFi.status() != WL_CONNECTED) {
+				delay(500);
+				DEBUG_INIT_PRINT(".");
 			}
+			String sIP = WiFi.localIP().toString();
+			DEBUG_INIT_PRINTLN("");
+			DEBUG_INIT_PRINT("Connected to ");
+			DEBUG_INIT_PRINTLN(ssid);
+			DEBUG_INIT_PRINT("IP address: ");
+			DEBUG_INIT_PRINTLN(sIP);
+		} else {
+			const char *ssid = "ESP-MPU";
+			const char *password = "thereisnospoon";
+
+			/* You can remove/add the password parameter if you want the AP to be open/secure. */
+			//	WiFi.softAP(ssid, password);
+			WiFi.enableAP(true);
+			WiFi.softAP(ssid);
+			IPAddress ip(192, 168, 4, 1); // this 3 lines for a fix IP-address
+			IPAddress gateway(192, 168, 4, 1);
+			IPAddress subnet(255, 255, 255, 0);
+
+			WiFi.softAPConfig(ip, gateway, subnet);
+
 		}
 	}
 }
